d3f954798bb0008b376d714045028273437c93a2
Rahul Sunar <rahulsunarrahul55@gmail.com>
2025-03-28 17:55:42 +0530

    firstss

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # DATABASE_URL = config('DATABASE_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://backend-1mfv.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
